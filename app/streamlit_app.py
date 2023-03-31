84b0aea67215cf25bfa6facc1acced23befa610e
Sara <sarabonati96@gmail.com>
2023-03-31 14:05:28 +0200

    update UI

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -21,39 +21,53 @@ client = init_connection()
 
 st.markdown('After connecting to mongoDB we can create a new database, called `workshop`,'
             'and within it a collection named `todos`')
+todos = client['workshop']['todos']
+if todos is not None:
+    st.success('Workshop database created, collection `todos` initialized!', icon="✅")
 
 example_todo = {"description": "Test post",
                  "category": "test",
                  "date_posted": datetime.datetime.utcnow()
                  }
 
-# CREATE AND READ
-todos = client['workshop']['todos']
-if todos is not None:
-    st.success('Workshop database created, collection `todos` initialized!', icon="✅")
 
-todos.insert_one(example_todo)
+col1, col2 = st.columns(2)
 
-# show existing documents in collection
-st.write("Existing TODOs: ")
-cursor = todos.find({})
-st.dataframe(pd.DataFrame(list(cursor)))
+with col1:
 
-# UPDATE
-st.subheader('Update TODO')
-with st.form('update', clear_on_submit = True):
-    id_to_update = st.text_input('Copy the id of todo you want to update')
-    doc = todos.find_one({"_id": id_to_update})
-    st.write(doc)
-    update = st.form_submit_button('Update')
-    if update:
-        todos.update_one({'_id': id_to_update}, { "$set": { 'description': 'Updated' } })
+    # INSERT
+    st.subheader('Insert TODO')
+    with st.form('insert', clear_on_submit = True):
+        todo_description = st.text_input('Description', 'Insert text here')
+        todo_category = st.selectbox('Select the todo category:', ('CHM', 'MIT', 'Harvard'))
+        insert = st.form_submit_button("Insert")
+        if insert:
+            todos.insert_one({"description": todo_description,
+                              "category": todo_category,
+                              "date_posted": datetime.datetime.utcnow()
+                            })
+
+    # UPDATE
+    st.subheader('Update TODO')
+    with st.form('update', clear_on_submit=True):
+        id_to_update = st.text_input('Copy the id of todo you want to update')
+        doc = todos.find_one({"_id": id_to_update})
+        st.write(doc)
+        update = st.form_submit_button('Update')
+        if update:
+            todos.update_one({'_id': id_to_update}, {"$set": {'description': 'Updated'}})
+
+    # DELETE
+    st.subheader('Delete TODO')
+    with st.form('delete', clear_on_submit=True):
+        id_to_delete = st.text_input('Copy the id of todo you want to update')
+        delete = st.form_submit_button('Delete')
+        if delete:
+            todos.delete_one({'_id': id_to_delete})
 
 
-# DELETE
-st.subheader('Delete TODO')
-with st.form('delete', clear_on_submit = True):
-    id_to_delete = st.text_input('Copy the id of todo you want to update')
-    delete = st.form_submit_button('Delete')
-    if delete:
-        todos.delete_one({'_id': id_to_delete})
+with col2:
+    # show existing documents in collection
+    st.write("Existing TODOs: ")
+    cursor = todos.find({})
+    st.dataframe(pd.DataFrame(list(cursor)), use_container_width=True)
